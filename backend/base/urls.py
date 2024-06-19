0d6ed55b4984b6c3bbebbd3bcc14e769dbd4f84e
LonlyEdward <lioneryedward1@gmail.com>
2024-06-20 00:14:25 +0300

    worked on the transfer feature

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView, TokenRefreshView,)
 from rest_framework.routers import DefaultRouter
-from .views import BudgetViewSet, BudgetEntryViewSet, ReminderViewSet, TransactionViewSet, AccountTypeViewSet, AccountViewSet, TransactionTypeViewSet, TransferViewSet
+from .views import BudgetViewSet, BudgetEntryViewSet, ReminderViewSet, TransactionViewSet, AccountTypeViewSet, AccountViewSet, TransactionTypeViewSet, TransferViewSet, TransferFunds
 
 router = DefaultRouter()
 router.register(r'budgets', BudgetViewSet)
@@ -19,6 +19,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('transfer/', TransferFunds.as_view(), name='transfer-funds'),
 
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
--- backend/base/views.py
+++ backend/base/views.py
@@ -3,18 +3,25 @@ from rest_framework.viewsets import ReadOnlyModelViewSet
 from .models import User, AccountType, Account, Transfer, TransactionType, Transaction, Budget, BudgetEntry, Reminder
 from .serializers import UserSerializer, AccountTypeSerializer, AccountSerializer, TransferSerializer, TransactionTypeSerializer, TransactionSerializer, BudgetSerializer, BudgetEntrySerializer, ReminderSerializer
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+# from rest_framework.decorators import api_view
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+# from .models import Account, Transfer
+from django.db import transaction
+# from .serializers import TransferSerializer
 
 
-@api_view(['GET'])
-def get_routes(request):
-    """returns a view containing all the possible routes"""
-    routes = [
-        '/api/token',
-        '/api/token/refresh'
-    ]
+# @api_view(['GET'])
+# def get_routes(request):
+#     """returns a view containing all the possible routes"""
+#     routes = [
+#         '/api/token',
+#         '/api/token/refresh'
+#     ]
 
-    return Response(routes)
+#     return Response(routes)
 
 
 class ReminderViewSet(viewsets.ModelViewSet):
@@ -55,3 +62,36 @@ class TransactionTypeViewSet(ReadOnlyModelViewSet):
 class AccountTypeViewSet(ReadOnlyModelViewSet):
     queryset = AccountType.objects.all()
     serializer_class = AccountTypeSerializer
+
+
+class TransferFunds(APIView):
+    def post(self, request):
+        serializer = TransferSerializer(data=request.data)
+        if serializer.is_valid():
+            from_account_id = serializer.validated_data['from_account']
+            to_account_id = serializer.validated_data['to_account']
+            amount = serializer.validated_data['amount']
+
+            try:
+                with transaction.atomic():
+                    from_account = Account.objects.select_for_update().get(id=from_account_id)
+                    to_account = Account.objects.select_for_update().get(id=to_account_id)
+
+                    if from_account.balance < amount:
+                        return Response({"error": "Insufficient funds"}, status=status.HTTP_400_BAD_REQUEST)
+
+                    from_account.balance -= amount
+                    to_account.balance += amount
+
+                    from_account.save()
+                    to_account.save()
+
+                    transfer = Transfer.objects.create(
+                        from_account=from_account,
+                        to_account=to_account,
+                        amount=amount
+                    )
+                return Response({"success": "Transfer completed"}, status=status.HTTP_200_OK)
+            except Account.DoesNotExist:
+                return Response({"error": "Account not found"}, status=status.HTTP_404_NOT_FOUND)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
